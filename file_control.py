74e7102123947a3e21bb3a1f1e2189bd6d030582
Arbishh <khatiwadaa3@lopers.unk.edu>
2024-12-15 20:50:23 +0000

    couldnt figure it out

--- file_control.py
+++ file_control.py
@@ -1,9 +1,39 @@
 # Create a function called load_tasks that reads tasks from a file
 # into a list and then returns the list.
+def load_tasks():
+ #open the file in read mode
+ file = open('tasks.txt','r')
+ # creatinf a list to hold records
+ task = []
+ #read first line
+ line = file.readline()
+ #loop for task if list is not empty
+ while line !='':
+     #secondary list for holding the temporary field
+     new_task =[]
+     #codes for adding and stripping new lines 1-4:
+     new_task.append(line.rstrip('\n'))
+     new_task.append(file.readline().rstrip("\n"))
+     new_task.append(file.readline().rstrip("\n"))
+     new_task.append(file.readline().rstrip("\n"))
+     task.append(new_task)#appending the fields 1-4 to task list
+     #restarting the loop
+     line = file.readline()
+ file.close()
 
 
 
 
 # Create a function called save_tasks that takes a list of tasks and 
 # writes them to a file for long non-volatile storage.
-
+def save_tasks(tasks):
+ #open file to append
+ with open('task.txt','a') as finaletask:
+ 
+      finaletask.write(tasks)
+      tasks.append(line.rstrip('\n'))
+      tasks(file.readline().rstrip('\n'))
+      tasks.append(file.readline().rstrip('\n'))
+      tasks.append(file.readline().rstrip('\n'))
+      tasks.append(new_task)
+      line = file.readline()
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import file_control
 
 def main():
   # Get tasks from file
-  tasks = load_tasks()
+  tasks = file_control.load_tasks()
 
   # Create loop for menu
   while True:
@@ -24,7 +24,7 @@ def main():
     elif choice == "3":
       complete(tasks)
     elif choice == "4":
-      save_tasks(tasks)
+      file_control.save_tasks(tasks)
       print("Thank you for using Task Tracker.")
       break
     else:
@@ -34,14 +34,57 @@ def main():
 
 # Create a function called display_tasks that takes a list of taks and
 # displays every task in the list.
+def display_tasks(tasks):
+ #display tasks on list with intervals
+ print(tasks)
+ return tasks
+def add_tasks():
+  #ask the user for new tasks
+  new_tasks =[]
+  Title = input("What task would you like to add?")
+  Description = input("Add description of your tasks:")
+  Date = input("Add the to-do date")
+  #put all into another list
+  new_tasks = [Title,Description,Date]
+
+  #add new task to the list via appending
+  test.append("new_tasks")
+  tasks.append(new_tasks)
+
+  #return the update list
+  return tasks
+
+#function for complete task
+def complete(tasks):
+  #display the 0 index  of each index in tasks
+  for title in tasks:
+    print(title[0])
+  #prompt user to choose task
+  utask = input("Which task would you like to mark as complete?")
+  if not utask.isdigit():
+    utask = input("Please choose a numbered task ")
+  if utask.isdigit():
+    index = int(utask) - 1
+    for i in range(tasks.len()):
+     #change the status to complete
+     tasks[index][3] = "complete"
+     break
+  
+
+ 
+  
+
+
+ 
+     
+  
+  
 
 
 
 
 
-# Create a function called add_task that takes a list of tasks, prompts
-# the user for another task, and then appends the new tasks to the 
-# end of the list.
+
 
 
 
